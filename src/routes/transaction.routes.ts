d4c0bb5f25be76499710fb6dd58a63050d14982d
Welisson luca <welissonluca17@gmail.com>
2021-08-14 08:20:33 -0300

    feat: add list all routes

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -16,7 +16,11 @@ interface Request {
 transactionRouter.get('/', (request, response) => {
   try {
     const transactions = transactionsRepository.all();
-    return response.json(transactions);
+    const balance = transactionsRepository.getBalance();
+    return response.json({
+      transactions,
+      balance,
+    });
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
